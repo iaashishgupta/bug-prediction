0d8a94c32557ce55aefd96d85ca8ccd63b415dcb
iaashishgupta <aashishgupta0409@gmail.com>
2020-10-14 12:35:23 +0530

    new file:   app/__init__.py 	new file:   app/base/__init__.py 	new file:   app/base/forms.py 	new file:   app/base/routes.py 	new file:   app/base/models.py 	new file:   app/base/static/assets/css/black-dashboard.css 	new file:   app/base/static/assets/css/black-dashboard.css.map 	new file:   app/base/static/assets/css/black-dashboard.min.css 	new file:   app/base/static/assets/css/nucleo-icons.css 	new file:   app/base/templates/base-site-rtl.html 	new file:   app/base/templates/base-site.html 	new file:   app/base/templates/errors/403.html 	new file:   app/base/templates/errors/404.html 	new file:   app/base/templates/errors/500.html 	new file:   app/base/templates/login/login.html 	new file:   app/base/templates/login/register.html 	new file:   app/base/templates/login/resultfail.html 	new file:   app/base/templates/login/resultpass.html 	new file:   app/base/templates/site_template/fixed-plugin.html 	new file:   app/base/templates/site_template/footer.html 	new file:   app/base/templates/site_template/navigation-rtl.html 	new file:   app/base/templates/site_template/navigation.html 	new file:   app/base/templates/site_template/scripts-sidebar.html 	new file:   app/base/templates/site_template/scripts.html 	new file:   app/base/templates/site_template/sidebar-rtl.html 	new file:   app/base/templates/site_template/sidebar.html 	new file:   app/base/util.py 	new file:   app/home/__init__.py 	new file:   app/home/routes.py 	new file:   app/home/templates/index.html 	new file:   app/home/templates/page-403.html 	new file:   app/home/templates/page-404.html 	new file:   app/home/templates/page-500.html 	new file:   app/home/templates/page-blank.html 	new file:   app/home/templates/page-rtl-support.html 	new file:   app/home/templates/page-user.html 	new file:   app/home/templates/ui-icons.html 	new file:   app/home/templates/ui-maps.html 	new file:   app/home/templates/ui-notifications.html 	new file:   app/home/templates/ui-tables.html 	new file:   app/home/templates/ui-typography.html 	new file:   app/model1.pkl 	new file:   app/research notebooks/.ipynb_checkpoints/CM1-checkpoint.ipynb 	new file:   run.py

---
+++ app/__init__.py
@@ -0,0 +1,68 @@
+import _pickle as pickle
+import numpy as np
+from flask import Flask, url_for
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from importlib import import_module
+from logging import basicConfig, DEBUG, getLogger, StreamHandler
+from os import path
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+		
+def register_extensions(app):
+    db.init_app(app)
+    login_manager.init_app(app)
+
+def register_blueprints(app):
+    for module_name in ('base', 'home'):
+        module = import_module('app.{}.routes'.format(module_name))
+        app.register_blueprint(module.blueprint)
+
+def configure_database(app):
+
+    @app.before_first_request
+    def initialize_database():
+        db.create_all()
+
+    @app.teardown_request
+    def shutdown_session(exception=None):
+        db.session.remove()
+		
+
+def configure_logs(app):
+    # soft logging
+    try:
+        basicConfig(filename='error.log', level=DEBUG)
+        logger = getLogger()
+        logger.addHandler(StreamHandler())
+    except:
+        pass
+
+def apply_themes(app):
+    def override_url_for():
+        return dict(url_for=_generate_url_for_theme)
+
+    def _generate_url_for_theme(endpoint, **values):
+        if endpoint.endswith('static'):
+            themename = values.get('theme', None) or \
+                app.config.get('DEFAULT_THEME', None)
+            if themename:
+                theme_file = "{}/{}".format(themename, values.get('filename', ''))
+                if path.isfile(path.join(app.static_folder, theme_file)):
+                    values['filename'] = theme_file
+        return url_for(endpoint, **values)
+
+def create_app(config, selenium=False):
+    app = Flask(__name__, static_folder='base/static')
+    app.config.from_object(config)
+    if selenium:
+        app.config['LOGIN_DISABLED'] = True
+    register_extensions(app)
+    register_blueprints(app)
+    configure_database(app)
+    configure_logs(app)
+    apply_themes(app)
+   # load_model()
+    return app
